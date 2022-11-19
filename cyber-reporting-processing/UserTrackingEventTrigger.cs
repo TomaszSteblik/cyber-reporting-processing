0e84630d702d4e223a470e6339026556b05893e7
rtrydev <r.kulka44@gmail.com>
2022-11-19 22:26:16 +0100

    Top 1 implementation of azure lambda or something

--- cyber-reporting-processing/UserTrackingEventTrigger.cs
+++ cyber-reporting-processing/UserTrackingEventTrigger.cs
@@ -1,17 +1,43 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using cyber_reporting_processing.Database;
+using cyber_reporting_processing.Enums;
+using cyber_reporting_processing.Extensions;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+using any = System.Collections.Generic.Dictionary<string, object>;
 
 namespace cyber_reporting_processing;
 
 public static class UserTrackingEventTrigger
 {
     [FunctionName("userTrackingEventTrigger")]
-    public static async Task RunAsync([ServiceBusTrigger("%QUEUE_NAME%", Connection = "CONNECTION_STRING_SERVICE_BUS")] string myQueueItem, ILogger log)
+    public static async Task RunAsync([ServiceBusTrigger("%QUEUE_NAME%", Connection = "CONNECTION_STRING_SERVICE_BUS")] string queueItem, ILogger log)
     {
-        log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+        var eventTypes = new EventTypes();
+
+        var parsedQueueItem = JsonConvert.DeserializeObject<any>(queueItem);
+        var eventName = parsedQueueItem["Event"];
+        var eventValue = parsedQueueItem["Value"].toAny();
         
+        var db = new DatabaseHandler();
+
+        var eventBody = new
+        {
+            id = Guid.NewGuid().ToString(),
+            user_id = eventValue["UserId"],
+            username = eventValue["Username"],
+            timestamp = eventValue["UnixTimeStamp"],
+            event_type = eventTypes[eventName as string]
+        };
+
+
+        var result = await db.AddItemToDatabase(eventBody);
+        
+        Console.WriteLine(result.StatusCode);
     }
 }
